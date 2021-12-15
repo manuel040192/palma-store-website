1e27ce734d0f86ecddbe8c07aa1f81a1448ff7fd
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-15 18:51:30 -0500

    Update Gallery_Women.js

--- src/components/Gallery_Women.js
+++ src/components/Gallery_Women.js
@@ -31,19 +31,19 @@ function Women() {
                     }}
                 >
                     <div style={{ position: 'relative' }}>
-                        <a href="#"><img src="images/8B5EAD68-0636-46E3-B16B-471049E9E657.jpg" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
+                        <Link to="/"><img src="images/8B5EAD68-0636-46E3-B16B-471049E9E657.jpg" alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 140, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                     <div style={{ position: 'relative' }}>
-                        <a href="#"><img src="images/BE57A173-2D4F-48D4-A681-96920081068F.jpeg" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
+                        <Link to="/"><img src="images/BE57A173-2D4F-48D4-A681-96920081068F.jpeg" alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 155, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                     <div style={{ position: 'relative' }}>
-                        <a href="#"><img src="images/24CED3EE-F1A9-4460-B9DA-ADD9ED9DEF41.jpeg" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
+                        <Link to="/"><img src="images/24CED3EE-F1A9-4460-B9DA-ADD9ED9DEF41.jpeg" alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 150, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                     <div style={{ position: 'relative' }}>
-                        <a href="#"><img src="images/788B8368-246D-4825-89CA-4557AECB0163.jpeg" style={{ width: 350, height: 310, objectFit: 'cover' }}/></a>
+                        <Link to="/"><img src="images/788B8368-246D-4825-89CA-4557AECB0163.jpeg" alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 150, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                 </div>
