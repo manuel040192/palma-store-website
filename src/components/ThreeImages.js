6b288c2994db1ad0f5737e8eb2a8f60ea029d1b3
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-15 18:53:20 -0500

    Update ThreeImages.js

--- src/components/ThreeImages.js
+++ src/components/ThreeImages.js
@@ -31,15 +31,15 @@ function ThreeImages() {
                     }}
                 >
                     <div style={{ position: 'relative' }}>
-                        <Link to="/women"><img src="images/A8C0C744-4A5C-46F6-9C8D-3B59573CE182.jpeg" style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
+                        <Link to="/women"><img src="images/A8C0C744-4A5C-46F6-9C8D-3B59573CE182.jpeg" alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <Link to="/women"><span style={{ position: 'absolute', top: 135, left: 140, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}>Women</span></Link>   
                     </div>
                     <div style={{ position: 'relative' }}>
-                        <Link to="/petsandowners"><img src="images/0A8E19A8-A8ED-48E9-B942-5C164EE487CD.jpeg" style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
+                        <Link to="/petsandowners"><img src="images/0A8E19A8-A8ED-48E9-B942-5C164EE487CD.jpeg" alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <Link to="/petsandowners"><span style={{ position: 'absolute', top: 135, left: 78, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}>Pets and Owners</span></Link>    
                     </div>
                     <div style={{ position: 'relative' }}>
-                        <Link to="/goods"><img src="images/IMG_2710.png" style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
+                        <Link to="/goods"><img src="images/IMG_2710.png" alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <Link to="/goods"><span style={{ position: 'absolute', top: 135, left: 150, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}>Goods</span></Link>    
                     </div>
                 </div>
