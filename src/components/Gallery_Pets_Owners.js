ce95a2178bd8179cf00f6b55d96536704a281b57
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-16 16:23:04 -0500

    Update Gallery_Pets_Owners.js

--- src/components/Gallery_Pets_Owners.js
+++ src/components/Gallery_Pets_Owners.js
@@ -32,19 +32,19 @@ function PetsAndOwners() {
                     }}
                 >
                     <div style={{ position: 'relative' }}>
-                        <Link to="/"><img src='images/0803F4BC-F267-4EB6-A8E0-880AA438DE25 2.jpeg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
+                        <Link to="${process.env.PUBLIC_URL}/"><img src='images/0803F4BC-F267-4EB6-A8E0-880AA438DE25 2.jpeg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 140, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                     <div style={{ position: 'relative' }}>
-                        <Link to="/"><img src='images/043CE7B4-AF79-4EEB-B282-B5DA75580663.jpeg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
+                        <Link to="${process.env.PUBLIC_URL}/"><img src='images/043CE7B4-AF79-4EEB-B282-B5DA75580663.jpeg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 155, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                     <div style={{ position: 'relative' }}>
-                        <Link to="/"><img src='images/4DE78DFB-F30B-4375-93FC-6FCE7D0B9F5B 2.jpeg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
+                        <Link to="${process.env.PUBLIC_URL}/"><img src='images/4DE78DFB-F30B-4375-93FC-6FCE7D0B9F5B 2.jpeg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 150, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                     <div style={{ position: 'relative' }}>
-                        <Link to="/"><img src='images/ADAFD1CE-6B9A-4B9B-AB72-721147941E34.jpeg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
+                        <Link to="${process.env.PUBLIC_URL}/"><img src='images/ADAFD1CE-6B9A-4B9B-AB72-721147941E34.jpeg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 150, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                 </div>
