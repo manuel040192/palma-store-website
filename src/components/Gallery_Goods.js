23b209c1b012038cec07098f24b0953b342ef838
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-16 17:13:05 -0500

    Update Gallery_Goods.js

--- src/components/Gallery_Goods.js
+++ src/components/Gallery_Goods.js
@@ -32,11 +32,11 @@ function Goods() {
                     }}
                 >
                     <div style={{ position: 'relative' }}>
-                        <Link to=`{process.env.PUBLIC_URL}/`><img src='images/IMG_2714.jpg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
+                        <Link to='{process.env.PUBLIC_URL}/'><img src='images/IMG_2714.jpg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 140, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                     <div style={{ position: 'relative' }}>
-                        <Link to=`{process.env.PUBLIC_URL}/`><img src='images/IMG_2711.jpg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
+                        <Link to='{process.env.PUBLIC_URL}/'><img src='images/IMG_2711.jpg' alt='' style={{ width: 350, height: 310, objectFit: 'cover' }}/></Link>
                         <span style={{ position: 'absolute', top: 135, left: 155, color: '#ffffff', fontFamily: 'Rajdhani', fontSize: 28, fontWeight: 400 }}></span>    
                     </div>
                 </div>
