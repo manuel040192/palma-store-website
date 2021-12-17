a2d0e87bea6246d158570a7afe7c3e7294b6d5c7
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-17 00:48:25 -0500

    Update RightNavMenu_Two_Login.js

--- src/components/RightNavMenu_Two_Login.js
+++ src/components/RightNavMenu_Two_Login.js
@@ -3,13 +3,8 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './Navbar_White_Two.css';
 import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
 
 export default function RightNavbarTwoLogin() {
     return (
