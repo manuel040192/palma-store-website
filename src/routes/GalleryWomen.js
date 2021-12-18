a17a531c3063a71cfdbc6cc93aaa1554c9b5be93
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-18 13:49:30 -0500

    Update GalleryWomen.js

--- src/routes/GalleryWomen.js
+++ src/routes/GalleryWomen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/Navbar_White_Two';
+import Header from '../components/Navbar';
 import Women from '../components/Gallery_Women';
 
 const WomenRoute = () => {
