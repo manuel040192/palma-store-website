acb795780a0bfccf0870844dbc2bacf2151dab45
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2021-12-18 13:45:53 -0500

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../components/Navbar_White_Two';
 import Banner from '../components/Banner';
-import ThreeImages from '../components/ThreeImages';
+import ProductTypeCovers from '../components/ProductTypeCovers';
 
 const HomeRoute = () => {
     return (
         <>
             <Header />
             <Banner />
-            <ThreeImages />       
+            <ProductTypeCovers />       
         </>
     )
 }
